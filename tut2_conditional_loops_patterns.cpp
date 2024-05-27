40995eae4b3317f7c88f522fd47bb91564f7e00e
Kaladhar <kaladhargopal@gmail.com>
2024-05-27 19:33:24 +0530

    started tut2

---
+++ tut2_conditional_loops_patterns.cpp
@@ -0,0 +1,16 @@
+//IN this tutorial conditional statements, loops while loop and patterns will be covered
+
+/*
+
+
+
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
