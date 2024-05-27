293169921f14d4619f245093767e47418d4c9b34
Kaladhar <kaladhargopal@gmail.com>
2024-05-27 20:39:45 +0530

    tut2...cont...

--- tut2_conditional_loops_patterns.cpp
+++ tut2_conditional_loops_patterns.cpp
@@ -1,7 +1,14 @@
 //IN this tutorial conditional statements, loops while loop and patterns will be covered
 
 /*
+<< and >> are called INsertion and Extraction operators
+<< - is used with cout to insert the data that has to be displayed on the screen to the output stream
 
+>> - is used to extract the object in cin which has been stored as the input from the terminal
+
+
+QUestions for conditional statements
+1. program to print whether no. is +ve -ve or zero
 
 
 
@@ -11,6 +18,14 @@
 using namespace std;
 
 int main(){
+    int x;
+    cout<<"Enter the number : "<<endl;
+    cin>> x;
+
+    if(x>0){cout<<"THe number is +ve";}
+    else if (x<0){cout<<"THe number is -ve";}
+    else{cout<<"THe number is zero"<<endl;}
+
     
     return 0;
 }
