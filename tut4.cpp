081d60a8fa4eb8be1e9f7a865e54230537935df4
Kaladhar <kaladhargopal@gmail.com>
2024-05-30 19:28:46 +0530

    Skipping leetcode problems for now and watched switch case and functions

---
+++ tut4.cpp
@@ -0,0 +1,10 @@
+//QUestions based on decimal and binary number system
+// To be done later
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    return 0;
+}
---
+++ tut5_switch_functions.cpp
@@ -0,0 +1,36 @@
+/*
+IN this tutorial we will learn about 'switch case statement' and 'functions'.
+No proper handwritten notes were provided for this lec. that's why summary separately added to the notes.
+
+Summary : 
+switch inside infinite loop
+why not continue inside switch ??
+calculator using switch case
+functions call stack
+call by value and call by reference
+
+Homework:
+no. of notes problem using switch case 
+
+questions :
+1.calculating nCr - make two functions, one for factorial and other for nCr
+2.even and odd
+3.coutning function
+4.prime number
+5.function to calculate number of set bits in a decimal number
+6.nth fibonaci number
+
+
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    
+    return 0;
+
+}
+
