877533213151fed7f76b03f23a2e0b62f06d2dc3
Kaladhar <kaladhargopal@gmail.com>
2024-06-05 19:51:41 +0530

    Binary search completed...

--- tut9_binary_search_and_interview_ques.cpp
+++ tut9_binary_search_and_interview_ques.cpp
@@ -204,35 +204,180 @@ Q3. Find the pivot element in the array
     cout<< "Pivot (min) value is : "<< arr[end] << endl;
 
 
-Doubt questions : What if an array is rotated for monotonic decreasing function ? -> This code doesn't work for it
-                : Here we are taking some elements from last then putting them in front, what if we do taking
-                  some elements from start and then putting them in last ? 
+    Doubt questions : What if an array is rotated for monotonic decreasing function? ->This code doesn't worfor it
+                    : Here we are taking some elements from last then putting them in front, what if we take
+                      some elements from start and then put them in last ? 
+
+Q 4. Searching an element in a sorted rotated array
+
+    int FindPivot(int arr[],int size){
+        int start = 0;
+        int end = size-1;
+
+        while(start<end){
+            int mid = start+(end-start)/2;
+
+            //You are in line 1 the move towards right
+            if(arr[mid]>arr[0]){
+                start = mid+1;
+            }
+            //You are in line 2 then move towards left
+            if(arr[mid]<arr[end]){
+                end = mid;
+            }
+        }
+        return start;
+    }
+
+    int binSearch(int ar[],int start=0,int size=0,int key=0){
+        int end=size-1;
+        while(start<=end){
+            int mid = start +(end-start)/2;
+            if(ar[mid]==key){return mid;}
+            else if(key>ar[mid]){
+                start = mid+1;
+            }
+            else{
+                end= mid-1;
+            }
+        }
+        return -1;
+
+    }
+
+    int main(){
+        int size = 7;
+        int arr[]={9,10,91,-2,3,4,5};
+        int key = 91;
+        int pivot = FindPivot(arr,size);
+        cout<< "Pivot (min) value is : "<< arr[pivot] << endl;
+        
+        if (arr[pivot]<=key && key<=arr[size-1]){
+            cout<<"Finding in lower line"<<endl;
+            cout<< "Found at : "<<binSearch(arr,pivot,size,key)<<endl;
+        }
+        else{
+            cout<< "Found at : "<<binSearch(arr,0,pivot+1,key)<<endl;
+        }
+
+
+    }
+
+Q 5. Finding square root of perfect squares
+
+    int key = 10000; // Whose square root has to be found
+    int arr[key];
+    int start =0;
+    int end = key-1;
+    //Creating an array of size of the key
+    for(int i=1;i<=key;i++){arr[i-1]=i;}
+
+
+    while(start<=end){
+        int mid = start + (end-start)/2;
+        if((arr[mid]*arr[mid]) == key){
+            cout<<"Root : "<<arr[mid]<<endl;
+            break;
+        }
+        else if(arr[mid]*arr[mid] > key){
+            end = mid-1;
+        }
+        else{
+            start = mid+1;
+        }
+    }
+
+
+Q5. Find integral part of square root of any number
+
+    My logic : The difference between the sqaures of two adjacent numbers if the sum of those two adjacent numbers
+               Therefore, while finding squares, if the difference of the square of the element in the array and the key is less than the sum of the element of array and its ajacent element 
+                                (but adjacent kaun sa?)
+
+
+    int key = 25; // Whose square root has to be found
+    int arr[key];
+    int start =0;
+    int end = key-1;
+    int ans = 0;
+
+    //Creating an array of size of the key
+    for(int i=1;i<=key;i++){arr[i-1]=i;}
+
+    // NOTE :
+    //instead of creating an array
+    // start = 0; end = key; would also work and in while loop instead of arr[mid] just use mid
+
+    while(start<=end){
+        int mid = start + (end-start)/2;
+        if((arr[mid]*arr[mid]) == key){
+            // cout<<"Root : "<<arr[mid]<<endl;
+            ans = arr[mid];
+            break;
+        }
+        else if(arr[mid]*arr[mid] > key){
+            end = mid-1;
+        }
+        else{
+            start = mid+1;
+            ans = arr[mid];
+        }
+    }
+
+    cout<< "Root : "<<ans<<endl;
+
+
+Q6. Finding the fractional part of the square root of a given number
+ Logic : here we will use bruteforce method and keep adding small values 
+        First keep adding 0.1 to the value then 0.01 and so on
+
+    int key=27;
+    int precision =6;
+    double factor = 1;
+    double ans = 5;//Storing the integral part of the answer here. USing above code 
+
+    for(int i=1;i<=precision;i++){
+        factor /= 10; // after each iteration divide by 10
+
+        //Relatively easier to understand by creating a while loop inside a for loop
+        while(true){
+            double temp_sq = (ans + factor)*(ans + factor);
+            if(temp_sq > key){
+                break;
+            }
+            else{
+                ans = ans + factor;
+            }
+        }
+    }
+    cout<<"Root : "<<ans<<endl;
+
+    
 
 */                                  
 
 #include<iostream>
 using namespace std;
 
-int main(){
-    int size = 7;
-    int arr[]={9,10,91,-2,3,4,5};
-    int start = 0;
-    int end = size-1;
-    
-    while(start<end){
-        int mid = start+(end-start)/2;
 
-        //You are in line 1 the move towards right
-        if(arr[mid]>arr[0]){
-            start = mid+1;
-        }
-        //You are in line 2 then move towards left
-        if(arr[mid]<arr[end]){
-            end = mid;
+int main(){
+    int key=27;
+    int precision =6;
+    double factor = 1;
+    double ans = 5;//Storing the integral part of the answer here. USing above code 
+
+    for(int i=1;i<=precision;i++){
+        factor /= 10;
+        while(true){
+            double temp_sq = (ans + factor)*(ans + factor);
+            if(temp_sq > key){
+                break;
+            }
+            else{
+                ans = ans + factor;
+            }
         }
     }
-
-    cout<< "Pivot (min) value is : "<< arr[end] << endl;
-
+    cout<<"Root : "<<ans<<endl;
 }
 
