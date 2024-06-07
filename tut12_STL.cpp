2dbacb3a6f41904b728dd8a795391a052be5d7ae
Kaladhar <kaladhargopal@gmail.com>
2024-06-07 20:26:29 +0530

    Commit kar raha hua saare changes guysss...

---
+++ tut12_STL.cpp
@@ -0,0 +1,25 @@
+/*
+In this tutorial we will learn about STL - Standard Template Library of C++
+
+1. We can iterate over an like this also just like (for i in list) in python
+int arr[] = {12,3,4,5,6,6,54,4};
+    for(int i : arr){
+        cout<<i<<" ";
+    }
+
+2.
+
+*/
+
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int arr[] = {12,3,4,5,6,6,54,4};
+    for(int i : arr){
+        cout<<i<<" ";
+    }
+
+    return 0;
+}
